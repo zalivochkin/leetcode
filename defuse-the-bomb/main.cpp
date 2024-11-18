e084a191dd491340c9608186e51940b06b308c28
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-11-18 12:57:07 +0300

    defuse-the-bomb

---
+++ defuse-the-bomb/main.cpp
@@ -0,0 +1,47 @@
+
+class Solution {
+public:
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+
+    vector<int> decrypt(vector<int>& code, int k) {
+        const auto n = code.size();
+        vector<int> res(n);
+        if (k == 0) {
+            fill(res.begin(), res.end(), 0);
+        } else if (k > 0) {
+            int sum = 0;
+            int indx;
+            for (auto i = 0; i < k; ++i) {
+                indx = i % n;
+                sum += code[indx];
+            }
+            for (auto i = 0; i < n; ++i) {
+                indx = (i + k) % n;
+                sum -= code[i];
+                sum += code[indx];
+                res[i] = sum;
+            }
+        } else {
+            k = 0 - k;
+            reverse(code.begin(), code.end());
+            int sum = 0;
+            int indx;
+            for (auto i = 0; i < k; ++i) {
+                indx = i % n;
+                sum += code[indx];
+            }
+            for (auto i = 0; i < n; ++i) {
+                indx = (i + k) % n;
+                sum -= code[i];
+                sum += code[indx];
+                res[i] = sum;
+            }
+            reverse(res.begin(), res.end());
+        }
+        return res;
+    }
+};
