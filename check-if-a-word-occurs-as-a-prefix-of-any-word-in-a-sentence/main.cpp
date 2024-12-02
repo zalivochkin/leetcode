a8451ca5df4c31f9340738eec8eebacb9c31df91
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-02 13:47:05 +0300

    check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence

---
+++ check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/main.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        const auto n = sentence.size();
+        const auto m = searchWord.size();
+        int res = 1;
+        int j;
+        for (auto i = 0; i < n; ++i) {
+            for (j = 0; j < m; ++j) {
+                if (sentence[i + j] == ' ' || sentence[i + j] != searchWord[j]) {
+                    i += j;
+                    break;
+                }
+            }
+            if (j == m) {
+                return res;
+            }
+            while (i < n && sentence[i] != ' ') {
+                ++i;
+            }
+            ++res;
+        }
+        return -1;
+    }
+};
