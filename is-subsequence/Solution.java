d288d3ef9fe25622b248dc818c05d045840810bf
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-18 14:39:07 +0300

    is-subsequence

---
+++ is-subsequence/Solution.java
@@ -0,0 +1,17 @@
+﻿import java.util.*;
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sIndx = 0;
+        int tIndx = 0;
+        while (sIndx < s.length() && tIndx < t.length()) {
+            if (s.charAt(sIndx) == t.charAt(tIndx)) {
+                ++sIndx;
+                ++tIndx;
+            } else {
+                ++tIndx;
+            }
+        }
+        return sIndx == s.length();
+    }
+}
