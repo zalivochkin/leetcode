385366e43583b9ec6da99970a6caaba18b70dc68
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-18 14:19:24 +0300

    best-time-to-buy-and-sell-stock

---
+++ best-time-to-buy-and-sell-stock/Solution.java
@@ -0,0 +1,17 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxP = 0;
+        int leftIndx = 0;
+        for (int rightIndx = 1; rightIndx < prices.length; ++rightIndx) {
+            if (prices[rightIndx] < prices[leftIndx]) {
+                leftIndx = rightIndx;
+            } else {
+                maxP = Math.max(maxP, prices[rightIndx] - prices[leftIndx]);
+            }
+        }
+        return maxP;
+    }
+}
