6de47d3976ff1055efa07761667a0873e7eea01b
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-18 18:54:41 +0300

    word-pattern

---
+++ word-pattern/Solution.java
@@ -0,0 +1,31 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> map = new HashMap<>();
+        Map<String, Character> rmap = new HashMap<>();
+        int chIndx = 0;
+        for (var word : s.split(" ")) {
+            if (chIndx == pattern.length()) {
+                return false;
+            }
+            if (map.containsKey(pattern.charAt(chIndx))) {
+                if (!word.equals(map.get(pattern.charAt(chIndx)))) {
+                    return false;
+                }
+            } else {
+                map.put(pattern.charAt(chIndx), word);
+            }
+            if (rmap.containsKey(word)) {
+                if (rmap.get(word) != pattern.charAt(chIndx)) {
+                    return false;
+                }
+            } else {
+                rmap.put(word, pattern.charAt(chIndx));
+            }
+            ++chIndx;
+        }
+        return chIndx == pattern.length();
+    }
+}
