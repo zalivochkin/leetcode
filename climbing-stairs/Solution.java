c6bb1c6452bd31c5a607223af7c74c5004b47b7c
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-19 13:27:28 +0300

    climbing-stairs

---
+++ climbing-stairs/Solution.java
@@ -0,0 +1,17 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n < 4) {
+            return n;
+        }
+        int[] dp = new int[]{1, 2};
+        for (int i = 2; i < n; ++i) {
+            int newCount = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = newCount;
+        }
+        return dp[1];
+    }
+}
