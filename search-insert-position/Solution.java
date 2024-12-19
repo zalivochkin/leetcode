2133cfbf4f3d2940d5fa8f6bed46313cfc30786e
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-19 14:10:52 +0300

    search-insert-position

---
+++ search-insert-position/Solution.java
@@ -0,0 +1,20 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int leftIndx = 0;
+        int rightIndx = nums.length - 1;
+        while (rightIndx >= leftIndx) {
+            int midIndx = leftIndx + (rightIndx - leftIndx) / 2;
+            if (nums[midIndx] == target) {
+                return midIndx;
+            } else if (nums[midIndx] > target) {
+                rightIndx = midIndx - 1;
+            } else {
+                leftIndx = midIndx + 1;
+            }
+        }
+        return leftIndx;
+    }
+}
