9c9288cdea85763b42416085c03e9a19f13ceaf3
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-19 18:48:53 +0300

    valid-parentheses

---
+++ valid-parentheses/Solution.java
@@ -0,0 +1,30 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (var ch : s.toCharArray()) {
+            if (st.isEmpty()) {
+                if (ch == ')' || ch == '}' || ch == ']') {
+                    return false;
+                }
+                st.push(ch);
+            } else {
+                if (ch == '(' || ch == '{' || ch == '[') {
+                    st.push(ch);
+                } else {
+                    if (ch == ')' && st.peek() != '(') {
+                        return false;
+                    } else if (ch == '}' && st.peek() != '{') {
+                        return false;
+                    } else if (ch == ']' && st.peek() != '[') {
+                        return false;
+                    }
+                    st.pop();
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
