7a557d377304b6722c67c2f312759213cd192b47
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-19 18:14:00 +0300

    minimum-absolute-difference-in-bst

---
+++ minimum-absolute-difference-in-bst/Solution.java
@@ -0,0 +1,36 @@
+﻿
+import java.util.*;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    ArrayList<Integer> sortedVals = new ArrayList<>();
+    void dsf(TreeNode root) {
+        if (root.left != null) {
+            dsf(root.left);
+        }
+        sortedVals.add(root.val);
+        if (root.right != null) {
+            dsf(root.right);
+        }
+    }
+    public int getMinimumDifference(TreeNode root) {
+        dsf(root);
+        int minDiff = Integer.MAX_VALUE;
+        for (var i = 1; i < sortedVals.size(); ++i) {
+            minDiff = Math.min(minDiff, sortedVals.get(i) - sortedVals.get(i - 1));
+        }
+        return minDiff;
+    }
+}
