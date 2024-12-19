e77a97989fcb578187a943f9e57a7442fc6d4a26
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-19 15:36:59 +0300

    permutations

---
+++ permutations/Solution.java
@@ -0,0 +1,28 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums.length == 1) {
+            ans.add(new ArrayList<>(List.of(nums[0])));
+        } else {
+            for (var i = 0; i < nums.length; ++i) {
+                int[] subNums = new int[nums.length - 1];
+                int k = 0;
+                for (var j = 0; j < nums.length; ++j) {
+                    if (j != i) {
+                        subNums[k] = nums[j];
+                        ++k;
+                    }
+                }
+                List<List<Integer>> subAns = permute(subNums);
+                for (var subA : subAns) {
+                    subA.add(0, nums[i]);
+                    ans.add(subA);
+                }
+            }
+        }
+        return ans;
+    }
+}
