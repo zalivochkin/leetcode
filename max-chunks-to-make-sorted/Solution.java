a3a45d8e7082c1395adc67bf1cb92a3a5c93c337
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-19 11:33:27 +0300

    max-chunks-to-make-sorted

---
+++ max-chunks-to-make-sorted/Solution.java
@@ -0,0 +1,19 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int chunks = 1;
+        int curMax = arr[0];
+        int[] sortedArr = arr.clone();
+        Arrays.sort(sortedArr);
+        for (var i = 0; i < arr.length - 1; ++i) {
+            curMax = Math.max(curMax, arr[i]);
+            if (curMax == sortedArr[i]) {
+                ++chunks;
+                curMax = arr[i + 1];
+            }
+        }
+        return chunks;
+    }
+}
