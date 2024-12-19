eac674107644a7d654be29f204f1267181286dd0
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-19 18:40:39 +0300

    merge-two-sorted-lists

---
+++ merge-two-sorted-lists/Solution.java
@@ -0,0 +1,30 @@
+﻿
+import java.util.*;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode root = new ListNode();
+        if (list1.val <= list2.val) {
+            root.val = list1.val;
+            root.next = mergeTwoLists(list1.next, list2);
+        } else {
+            root.val = list2.val;
+            root.next = mergeTwoLists(list1, list2.next);
+        }
+        return root;
+    }
+}
