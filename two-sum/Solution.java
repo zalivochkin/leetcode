cebb8e4b71c81eb254f4c58205bedfbfe1ec1124
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-19 19:14:25 +0300

    two-sum

---
+++ two-sum/Solution.java
@@ -0,0 +1,19 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (var i = 0; i < nums.length; ++i) {
+            if (mp.isEmpty()) {
+                mp.put(nums[i], i);
+            } else {
+                if (mp.containsKey(target - nums[i])) {
+                    return new int[]{mp.get(target - nums[i]), i};
+                }
+                mp.put(nums[i], i);
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
