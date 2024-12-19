fe0592e4a9dea5a73bba7bb04f58da486fdc03be
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-19 13:38:07 +0300

    sqrtx

---
+++ sqrtx/Solution.java
@@ -0,0 +1,21 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int mySqrt(int x) {
+        long leftY = 0;
+        long rightY = x / 2;
+        while (rightY - leftY > 1) {
+            long midY = leftY + (rightY - leftY) / 2;
+            if (midY * midY > x) {
+                rightY = midY - 1;
+            } else {
+                leftY = midY;
+            }
+        }
+        if ((leftY + 1) * (leftY + 1) <= x) {
+            return (int) leftY + 1;
+        }
+        return (int) leftY;
+    }
+}
