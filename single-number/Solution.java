c79bf62f885baab1e3820a4af5b3524a5e1c864f
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-19 13:43:23 +0300

    single-number

---
+++ single-number/Solution.java
@@ -0,0 +1,12 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (var num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+}
