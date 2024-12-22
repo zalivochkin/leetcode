ee4b4eea515067a7dba198faec503ad693c08244
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2024-12-22 09:49:25 +0300

    find-building-where-alice-and-bob-can-meet

---
+++ find-building-where-alice-and-bob-can-meet/Solution.java
@@ -0,0 +1,67 @@
+﻿
+import java.util.*;
+
+class Solution {
+    class BinaryIndexedTree {
+        private final int inf = 1 << 30;
+        private final int n;
+        private final int[] c;
+
+        public BinaryIndexedTree(int n) {
+            this.n = n;
+            c = new int[n + 1];
+            Arrays.fill(c, inf);
+        }
+
+        public void update(int x, int v) {
+            while (x <= n) {
+                c[x] = Math.min(c[x], v);
+                x += x & -x;
+            }
+        }
+
+        public int query(int x) {
+            int mi = inf;
+            while (x > 0) {
+                mi = Math.min(mi, c[x]);
+                x -= x & -x;
+            }
+            return mi == inf ? -1 : mi;
+        }
+    }
+    public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
+        var n = heights.length;
+        var m = queries.length;
+        for (var i = 0; i < m; ++i) {
+            if (queries[i][0] > queries[i][1]) {
+                queries[i] = new int[] {queries[i][1], queries[i][0]};
+            }
+        }
+        Integer[] idx = new Integer[m];
+        for (var i = 0; i < m; ++i) {
+            idx[i] = i;
+        }
+        Arrays.sort(idx, (i, j) -> queries[j][1] - queries[i][1]);
+        var s = heights.clone();
+        Arrays.sort(s);
+        var ans = new int[m];
+        var j = n - 1;
+        BinaryIndexedTree tree = new BinaryIndexedTree(n);
+        for (var i : idx) {
+            var l = queries[i][0];
+            var r = queries[i][1];
+            while (j > r) {
+                var k = n - Arrays.binarySearch(s, heights[j]) + 1;
+                tree.update(k, j);
+                --j;
+            }
+            if (l == r || heights[l] < heights[r]) {
+                ans[i] = r;
+            } else {
+                var k = n - Arrays.binarySearch(s, heights[l]);
+                ans[i] = tree.query(k);
+            }
+        }
+        return ans;
+    }
+}
--- find-building-where-alice-and-bob-can-meet/main.cpp
+++ find-building-where-alice-and-bob-can-meet/main.cpp
@@ -1,46 +1,77 @@
 
+class BinaryIndexedTree {
+private:
+    int inf = 1 << 30;
+    int n;
+    vector<int> c;
+
+public:
+    explicit BinaryIndexedTree(int n) : n{n} {
+        c.resize(n + 1, inf);
+    }
+
+    void update(int x, int v) {
+        while (x <= n) {
+            c[x] = min(c[x], v);
+            x += x & -x;
+        }
+    }
+
+    int query(int x) {
+        int mi = inf;
+        while (x > 0) {
+            mi = min(mi, c[x]);
+            x -= x & -x;
+        }
+        return mi == inf ? -1 : mi;
+    }
+};
+
 class Solution {
 public:
-    vector<int> leftmostBuildingQueries(vector<int> &heights, vector<vector<int> > &queries) {
-        int n = heights.size();
-        vector<int> rm(n, -1);
-        stack<int> s;
-        for (int i = n - 1; i >= 0; i--) {
-            while (!s.empty() && heights[s.top()] <= heights[i]) {
-                s.pop();
-            }
-            if (!s.empty()) {
-                rm[i] = s.top();
-            }
-            s.push(i);
-        }
-        int q = queries.size();
-        vector<int> ans(q, -1);
-        for (int i = 0; i < q; i++) {
-            int x = queries[i][0];
-            int y = queries[i][1];
-            if (y > x) {
-                swap(x, y);
-            }
-            if (x == y) {
-                ans[i] = x;
-            } else if (x > y) {
-                if (heights[x] > heights[y]) {
-                    ans[i] = x;
-                } else {
-                    if (rm[y] == -1) {
-                        ans[i] = -1;
-                    } else {
-                        int p = rm[x];
-                        while (p != -1 && heights[p] <= heights[y]) {
-                            p = rm[p];
-                        }
-                        ans[i] = p;
-                    }
-                }
-            }
-        }
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
 
+    vector<int> leftmostBuildingQueries(vector<int> &heights, vector<vector<int> > &queries) {
+        auto n = heights.size();
+        auto m = queries.size();
+        for (auto& q : queries) {
+            if (q[0] > q[1]) {
+                swap(q[0], q[1]);
+            }
+        }
+        vector<int> idx(m);
+        iota(idx.begin(), idx.end(), 0);
+        sort(idx.begin(), idx.end(), [&](int i, int j) {
+            return queries[j][1] < queries[i][1];
+        });
+        vector<int> s = heights;
+        sort(s.begin(), s.end());
+        s.erase(unique(s.begin(), s.end()), s.end());
+        vector<int> ans(m);
+        auto j = n - 1;
+        BinaryIndexedTree tree(n);
+        int l;
+        int r;
+        int k;
+        for (auto i : idx) {
+            l = queries[i][0];
+            r = queries[i][1];
+            while (j > r) {
+                k = s.end() - lower_bound(s.begin(), s.end(), heights[j]) + 1;
+                tree.update(k, j);
+                --j;
+            }
+            if (l == r || heights[l] < heights[r]) {
+                ans[i] = r;
+            } else {
+                k = s.end() - lower_bound(s.begin(), s.end(), heights[l]);
+                ans[i] = tree.query(k);
+            }
+        }
         return ans;
     }
 };
