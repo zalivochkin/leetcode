5ca88a967f54c87ddf262df0017e5952498a5c0e
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-01-16 10:30:04 +0300

    bitwise-xor-of-all-pairings

---
+++ bitwise-xor-of-all-pairings/Solution.java
@@ -0,0 +1,19 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int res = 0;
+        if (nums2.length % 2 == 1) {
+            for (var num : nums1) {
+                res ^= num;
+            }
+        }
+        if (nums1.length % 2 == 1) {
+            for (var num : nums2) {
+                res ^= num;
+            }
+        }
+        return res;
+    }
+}
---
+++ bitwise-xor-of-all-pairings/main.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int res = 0;
+        if (nums2.size() % 2 == 1) {
+            for (auto num : nums1) {
+                res ^= num;
+            }
+        }
+        if (nums1.size() % 2 == 1) {
+            for (auto num : nums2) {
+                res ^= num;
+            }
+        }
+        return res;
+    }
+};
