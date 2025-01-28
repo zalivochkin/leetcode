6ae323823f6114b810354a2f4581d016ae151234
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-01-28 21:51:43 +0300

    kth-smallest-element-in-a-bst

---
+++ kth-smallest-element-in-a-bst/Solution.java
@@ -0,0 +1,39 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    int global_k = 0;
+    int res = 0;
+    public void dfs(TreeNode root) {
+        if (global_k <= 0) {
+            return;
+        }
+        if (root.left != null) {
+            dfs(root.left);
+        }
+        --global_k;
+        if (global_k == 0) {
+            res = root.val;
+        }
+        if (root.right != null) {
+            dfs(root.right);
+        }
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        global_k = k;
+        dfs(root);
+        return res;
+    }
+}
