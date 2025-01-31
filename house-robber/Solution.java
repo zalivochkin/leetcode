ba8593c365f8af3b6d3e5c657bf54dfad39af9dc
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-01-31 18:06:43 +0300

    house-robber

---
+++ house-robber/Solution.java
@@ -0,0 +1,18 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int rob(int[] nums) {
+        final var n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (var i = 2; i < n; ++i) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[n - 1];
+    }
+}
