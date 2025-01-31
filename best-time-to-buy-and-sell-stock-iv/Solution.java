0ae1c1adab5b58720e54eb19e15b6c15ce1854fd
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-01-31 19:09:13 +0300

    best-time-to-buy-and-sell-stock-iv

---
+++ best-time-to-buy-and-sell-stock-iv/Solution.java
@@ -0,0 +1,19 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        final var n = prices.length;
+        int[][] dp = new int[k + 1][2];
+        for (var i = 1; i <= k; ++i) {
+            dp[i][1] = -prices[0];
+        }
+        for (var i = 1; i < n; ++i) {
+            for (int j = k; j > 0; --j) {
+                dp[j][0] = Math.max(dp[j][1] + prices[i], dp[j][0]);
+                dp[j][1] = Math.max(dp[j - 1][0] - prices[i], dp[j][1]);
+            }
+        }
+        return dp[k][0];
+    }
+}
