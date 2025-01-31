5968a7916040935f4945697b66d3b89ea00b5c1c
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-01-31 16:58:24 +0300

    reverse-bits

---
+++ reverse-bits/Solution.java
@@ -0,0 +1,14 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for (var i = 0; i < 32; ++i) {
+            res = res << 1;
+            res |= (n & 1);
+            n = n >> 1;
+        }
+        return res;
+    }
+}
