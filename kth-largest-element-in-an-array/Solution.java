a46e2ffaa3d6459c2ca9fa2b92cb61b73bffb29e
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-01-31 16:46:56 +0300

    kth-largest-element-in-an-array

---
+++ kth-largest-element-in-an-array/Solution.java
@@ -0,0 +1,15 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(nums.length, Collections.reverseOrder());
+        for (var num : nums) {
+            pq.add(num);
+        }
+        for (var i = 1; i < k; ++i) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
