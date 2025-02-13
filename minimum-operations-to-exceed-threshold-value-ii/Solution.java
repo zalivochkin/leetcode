0d459d3770b26999233a72b8f4b5847fec12f01b
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-02-13 10:35:39 +0300

    minimum-operations-to-exceed-threshold-value-ii

---
+++ minimum-operations-to-exceed-threshold-value-ii/Solution.java
@@ -0,0 +1,19 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        for (var num : nums) {
+            pq.offer((long) num);
+        }
+        int ops = 0;
+        while (pq.size() > 1 && pq.peek() < k) {
+            long x = pq.poll();
+            long y = pq.poll();
+            pq.offer(Math.min(x, y) * 2 + Math.max(x, y));
+            ++ops;
+        }
+        return ops;
+    }
+}
