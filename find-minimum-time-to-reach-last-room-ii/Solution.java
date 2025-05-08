b313732768a5c81b09cd772a51499fb50facfd04
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-05-08 07:25:31 +0300

    find-minimum-time-to-reach-last-room-ii

---
+++ find-minimum-time-to-reach-last-room-ii/Solution.java
@@ -0,0 +1,40 @@
+﻿
+import java.util.*;
+
+class Solution {
+    public int minTimeToReach(int[][] moveTime) {
+        final var n = moveTime.length;
+        final var m = moveTime[0].length;
+        int[][] dist = new int[n][m];
+        for (var row : dist) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        dist[0][0] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.offer(new int[] {0, 0, 0});
+        int[] dirs = {-1, 0, 1, 0, -1};
+        while (true) {
+            int[] p = pq.poll();
+            int d = p[0];
+            int i = p[1];
+            int j = p[2];
+            if (i == n - 1 && j == m - 1) {
+                return d;
+            }
+            if (d > dist[i][j]) {
+                continue;
+            }
+            for (var k = 0; k < 4; k++) {
+                int ik = i + dirs[k];
+                int jk = j + dirs[k + 1];
+                if (ik >= 0 && ik < n && jk >= 0 && jk < m) {
+                    int t = Math.max(moveTime[ik][jk], dist[i][j]) + 1;
+                    if (dist[ik][jk] > t) {
+                        dist[ik][jk] = t;
+                        pq.offer(new int[] {t, ik, jk});
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ find-minimum-time-to-reach-last-room-ii/main.cpp
@@ -0,0 +1,37 @@
+
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        const auto n = moveTime.size();
+        const auto m = moveTime[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        dist[0][0] = 0;
+        priority_queue<array<int, 3>, vector<array<int, 3>>, greater<>> pq;
+        pq.push({0, 0, 0});
+        vector<int> dirs{-1, 0, 1, 0, -1};
+        int ik;
+        int jk;
+        int t;
+        while (true) {
+            auto [d, i, j] = pq.top();
+            pq.pop();
+            if (i == n - 1 && j == m - 1) {
+                return d;
+            }
+            if (d > dist[i][j]) {
+                continue;
+            }
+            for (auto k = 0; k < 4; ++k) {
+                ik = i + dirs[k];
+                jk = j + dirs[k + 1];
+                if (ik >= 0 && ik < n && jk >= 0 && jk < m) {
+                    t = max(moveTime[ik][jk], dist[i][j]) + (i + j) % 2 + 1;
+                    if (dist[ik][jk] > t) {
+                        dist[ik][jk] = t;
+                        pq.push({t, ik, jk});
+                    }
+                }
+            }
+        }
+    }
+};
