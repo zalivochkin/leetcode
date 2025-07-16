05401fb9f431260af2ce9e3edaa8d51171d21b7d
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-07-16 16:32:24 +0300

    find-the-maximum-length-of-valid-subsequence-i

---
+++ find-the-maximum-length-of-valid-subsequence-i/Solution.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public int maximumLength(int[] nums) {
+        int k = 2;
+        int[][] dp = new int[k][k];
+        int ans = 0;
+        for (var num : nums) {
+            num %= k;
+            for (var j = 0; j < k; ++j) {
+                int y = (j - num + k) % k;
+                dp[num][y] = dp[y][num] + 1;
+                ans = Math.max(ans, dp[num][y]);
+            }
+        }
+        return ans;
+    }
+}
