89e637008904e88dc55f06c4c112d2ad74c0b22b
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-08-10 09:54:14 +0300

    reordered-power-of-2

---
+++ reordered-power-of-2/Solution.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        String s = convert(n);
+        for (int i = 1; i <= Math.pow(10, 9); i <<= 1) {
+            if (s.equals(convert(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String convert(int n) {
+        char[] cnt = new char[10];
+        while (n > 0) {
+            cnt[n % 10] += 1;
+            n /= 10;
+        }
+        return new String(cnt);
+    }
+}
---
+++ reordered-power-of-2/main.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        vector<int> s = convert(n);
+        for (int i = 1; i <= pow(10, 9); i <<= 1) {
+            if (s == convert(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static vector<int> convert(int n) {
+        vector<int> cnt(10);
+        while (n > 0) {
+            cnt[n % 10] += 1;
+            n /= 10;
+        }
+        return cnt;
+    }
+};
