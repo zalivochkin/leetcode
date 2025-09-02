8bb2b39f8df77e4e875b7d315888e54ffbcd80ba
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-09-02 09:19:58 +0300

    find-the-number-of-ways-to-place-people-i

---
+++ find-the-number-of-ways-to-place-people-i/main.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0] || (a[0] == b[0] && b[1] < a[1]);
+        });
+        const auto n = points.size();
+        int ans = 0;
+        for (auto i = 0; i < n; ++i) {
+            int y1 = points[i][1];
+            int maxY = INT_MIN;
+            for (auto j = i + 1; j < n; ++j) {
+                int y2 = points[j][1];
+                if (maxY < y2 && y2 <= y1) {
+                    maxY = y2;
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
