cb25083b1b461f65f7c66602f51ea3320a397cb0
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-10-25 05:07:45 +0300

    calculate-money-in-leetcode-bank

---
+++ calculate-money-in-leetcode-bank/Solution.java
@@ -0,0 +1,8 @@
+
+class Solution {
+    public int totalMoney(int n) {
+        int a = n / 7;
+        int b = n % 7;
+        return (28 + 28 + 7 * (a - 1)) * a / 2 + (a * 2 + b + 1) * b / 2;
+    }
+}
