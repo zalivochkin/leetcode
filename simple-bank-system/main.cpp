734edbd0d2e38810e004c06bd6fa1817f616209b
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-10-26 11:08:52 +0300

    simple-bank-system

---
+++ simple-bank-system/main.cpp
@@ -0,0 +1,36 @@
+
+class Bank {
+public:
+    vector<long long> balance;
+    int n;
+
+    Bank(vector<long long>& balance) {
+        this->balance = balance;
+        n = balance.size();
+    }
+
+    bool transfer(int account1, int account2, long long money) {
+        if (account1 > n || account2 > n || balance[account1 - 1] < money) {
+            return false;
+        }
+        balance[account1 - 1] -= money;
+        balance[account2 - 1] += money;
+        return true;
+    }
+
+    bool deposit(int account, long long money) {
+        if (account > n) {
+            return false;
+        }
+        balance[account - 1] += money;
+        return true;
+    }
+
+    bool withdraw(int account, long long money) {
+        if (account > n || balance[account - 1] < money) {
+            return false;
+        }
+        balance[account - 1] -= money;
+        return true;
+    }
+};
