884012c05b6553135873e84e049a9553d85c48c0
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-11-19 09:12:32 +0300

    keep-multiplying-found-values-by-two

---
+++ keep-multiplying-found-values-by-two/main.cpp
@@ -0,0 +1,11 @@
+
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        while (s.contains(original)) {
+            original *= 2;
+        }
+        return original;
+    }
+};
