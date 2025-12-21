ee7f01f320c7d8b5888d6ad35d9e95b9ff9901c8
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2025-12-21 11:31:45 +0300

    delete-columns-to-make-sorted-ii

---
+++ delete-columns-to-make-sorted-ii/Solution.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int n = strs.length;
+        int m = strs[0].length();
+        boolean[] st = new boolean[n - 1];
+        int ans = 0;
+        for (int j = 0; j < m; ++j) {
+            boolean mustDel = false;
+            for (int i = 0; i < n - 1; ++i) {
+                if (!st[i] && strs[i].charAt(j) > strs[i + 1].charAt(j)) {
+                    mustDel = true;
+                    break;
+                }
+            }
+            if (mustDel) {
+                ++ans;
+            } else {
+                for (int i = 0; i < n - 1; ++i) {
+                    if (!st[i] && strs[i].charAt(j) < strs[i + 1].charAt(j)) {
+                        st[i] = true;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
