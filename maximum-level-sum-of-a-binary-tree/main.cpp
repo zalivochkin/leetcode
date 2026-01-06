5b786fb2e83fb0b9c02db01e79e06e96ffbe5bf7
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2026-01-06 11:25:57 +0300

    maximum-level-sum-of-a-binary-tree

---
+++ maximum-level-sum-of-a-binary-tree/main.cpp
@@ -0,0 +1,39 @@
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        queue<TreeNode*> q{{root}};
+        int mx = numeric_limits<int>::min();
+        int ans = 0;
+        int i = 0;
+        while (!q.empty()) {
+            ++i;
+            int s = 0;
+            for (int n = q.size(); n; --n) {
+                root = q.front();
+                q.pop();
+                s += root->val;
+                if (root->left) {
+                    q.push(root->left);
+                }
+                if (root->right) {
+                    q.push(root->right);
+                }
+            }
+            if (mx < s) {
+                mx = s;
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};
