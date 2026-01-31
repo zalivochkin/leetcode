3a7f32330453216eb60781f13105491bccdc2c2f
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2026-01-31 10:04:20 +0300

    find-smallest-letter-greater-than-target

---
+++ find-smallest-letter-greater-than-target/Solution.java
@@ -0,0 +1,8 @@
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int i = Arrays.binarySearch(letters, (char) (target + 1));
+        i = i < 0 ? -i - 1 : i;
+        return letters[i % letters.length];
+    }
+}
---
+++ find-smallest-letter-greater-than-target/main.cpp
@@ -0,0 +1,8 @@
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int i = upper_bound(letters.begin(), letters.end(), target) - letters.begin();
+        return letters[i % letters.size()];
+    }
+};
