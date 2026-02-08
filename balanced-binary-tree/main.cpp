f9cc8d6aba3ac55df2422c51f48bfd550e59027a
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2026-02-08 09:23:14 +0300

    balanced-binary-tree

---
+++ balanced-binary-tree/main.cpp
@@ -0,0 +1,29 @@
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+    int height(const TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        int l = height(root->left);
+        int r = height(root->right);
+        if (l == -1 || r == -1 || abs(l - r) > 1) {
+            return -1;
+        }
+        return 1 + max(l, r);
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return height(root) >= 0;
+    }
+};
