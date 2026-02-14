99c791a9774f00d00f0cf4e15a556785be8a2c66
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2026-02-14 10:02:30 +0300

    champagne-tower

---
+++ champagne-tower/main.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        double dp[101][101] = {0.0};
+        dp[0][0] = poured;
+        for (int i = 0; i <= query_row; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                if (dp[i][j] > 1) {
+                    double half = (dp[i][j] - 1) / 2.0;
+                    dp[i][j] = 1;
+                    dp[i + 1][j] += half;
+                    dp[i + 1][j + 1] += half;
+                }
+            }
+        }
+        return dp[query_row][query_glass];
+    }
+};
