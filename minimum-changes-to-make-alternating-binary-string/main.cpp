c06933a9cab0e5feec3d7e98fdc26d843003c272
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2026-03-06 10:12:56 +0300

    check-if-binary-string-has-at-most-one-segment-of-ones minimum-changes-to-make-alternating-binary-string

---
+++ minimum-changes-to-make-alternating-binary-string/main.cpp
@@ -0,0 +1,12 @@
+
+class Solution {
+public:
+    int minOperations(string s) {
+        int cnt = 0;
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            cnt += s[i] != "01"[i & 1];
+        }
+        return min(cnt, n - cnt);
+    }
+};
