e3735510aa6c2481245bdf9cbad8f3f6ef1f2d67
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2026-04-14 08:54:06 +0300

    minimum-total-distance-traveled

---
+++ minimum-total-distance-traveled/main.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+public:
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        sort(robot.begin(), robot.end());
+        sort(factory.begin(), factory.end());
+        vector<vector<long long>> f(robot.size(), vector<long long>(factory.size()));
+        function<long long(int i, int j)> dfs = [&](int i, int j) -> long long {
+            if (i == robot.size()) {
+                return 0;
+            }
+            if (j == factory.size()) {
+                return 1e15;
+            }
+            if (f[i][j]) {
+                return f[i][j];
+            }
+            long long ans = dfs(i, j + 1);
+            long long t = 0;
+            for (int k = 0; k < factory[j][1]; ++k) {
+                if (i + k >= robot.size()) {
+                    break;
+                }
+                t += abs(robot[i + k] - factory[j][0]);
+                ans = min(ans, t + dfs(i + k + 1, j + 1));
+            }
+            f[i][j] = ans;
+            return ans;
+        };
+        return dfs(0, 0);
+    }
+};
