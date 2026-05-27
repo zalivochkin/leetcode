0009cc57310e3e77252cba3a297e6325ab2d4faf
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2026-05-27 09:18:52 +0300

    count-the-number-of-special-characters-ii

---
+++ count-the-number-of-special-characters-ii/Solution.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int[] first = new int['z' + 1];
+        int[] last = new int['z' + 1];
+        for (int i = 1; i <= word.length(); ++i) {
+            int j = word.charAt(i - 1);
+            if (first[j] == 0) {
+                first[j] = i;
+            }
+            last[j] = i;
+        }
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            int a = 'a' + i;
+            int b = 'A' + i;
+            if (last[a] > 0 && last[a] < first[b]) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
