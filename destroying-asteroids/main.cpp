cdcbc95d83fbe6776bb5293cc06c5b1a19c03f8c
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2026-05-31 10:45:33 +0300

    destroying-asteroids

---
+++ destroying-asteroids/main.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        ranges::sort(asteroids);
+        long long m = mass;
+        for (int x : asteroids) {
+            if (m < x) {
+                return false;
+            }
+            m += x;
+        }
+        return true;
+    }
+};
