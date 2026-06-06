2fe2fc65647dedbd662121a2452b419be5eab813
Zalivochkin Alexandr <zalivochkin@yandex.ru>
2026-06-06 08:52:05 +0300

    left-and-right-sum-differences

---
+++ left-and-right-sum-differences/Solution.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int l = 0, r = 0;
+        for (int x : nums) {
+            r += x;
+        }
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; ++i) {
+            r -= nums[i];
+            ans[i] = Math.abs(l - r);
+            l += nums[i];
+        }
+        return ans;
+    }
+}
---
+++ left-and-right-sum-differences/main.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int l = 0, r = 0;
+        for (int x : nums) {
+            r += x;
+        }
+        int n = nums.size();
+        vector<int> ans(n);
+        for (int i = 0; i < n; ++i) {
+            r -= nums[i];
+            ans[i] = abs(l - r);
+            l += nums[i];
+        }
+        return ans;
+    }
+};
